fc0463411703b5e00804821aebbc437d737da8f2
wnz <wnz_99@hust.edu.cn>
2020-09-12 14:02:48 +0800

    定时器中断

---
+++ 03_blink_timer_int.X/main.c
@@ -0,0 +1,115 @@
+/*
+ * File:   main.c
+ * Author: QuickFox
+ *
+ * Created on 2020?1?19?, ??5:16
+ */
+
+#include <xc.h>
+#ifndef BOOTLOADER
+// PIC16F18854 Configuration Bit Settings
+
+// 'C' source line config statements
+
+// CONFIG1
+#pragma config FEXTOSC = OFF   // External Oscillator mode selection bits (Oscillator not enabled)
+#pragma config RSTOSC = HFINT1 // Power-up default value for COSC bits (HFINTOSC (1MHz))
+#pragma config CLKOUTEN = OFF  // Clock Out Enable bit (CLKOUT function is disabled; i/o or oscillator function on OSC2)
+#pragma config CSWEN = ON      // Clock Switch Enable bit (Writing to NOSC and NDIV is allowed)
+#pragma config FCMEN = OFF     // Fail-Safe Clock Monitor Enable bit (FSCM timer disabled)
+
+// CONFIG2
+#pragma config MCLRE = ON    // Master Clear Enable bit (MCLR pin is Master Clear function)
+#pragma config PWRTE = OFF   // Power-up Timer Enable bit (PWRT disabled)
+#pragma config LPBOREN = OFF // Low-Power BOR enable bit (ULPBOR disabled)
+#pragma config BOREN = ON    // Brown-out reset enable bits (Brown-out Reset Enabled, SBOREN bit is ignored)
+#pragma config BORV = LO     // Brown-out Reset Voltage Selection (Brown-out Reset Voltage (VBOR) set to 1.9V on LF, and 2.45V on F Devices)
+#pragma config ZCD = OFF     // Zero-cross detect disable (Zero-cross detect circuit is disabled at POR.)
+#pragma config PPS1WAY = OFF // Peripheral Pin Select one-way control (The PPSLOCK bit can be set and cleared repeatedly by software)
+#pragma config STVREN = ON   // Stack Overflow/Underflow Reset Enable bit (Stack Overflow or Underflow will cause a reset)
+
+// CONFIG3
+#pragma config WDTCPS = WDTCPS_31 // WDT Period Select bits (Divider ratio 1:65536; software control of WDTPS)
+#pragma config WDTE = SWDTEN      // WDT operating mode (WDT enabled/disabled by SWDTEN bit in WDTCON0)
+#pragma config WDTCWS = WDTCWS_7  // WDT Window Select bits (window always open (100%); software control; keyed access not required)
+#pragma config WDTCCS = SC        // WDT input clock selector (Software Control)
+
+// CONFIG4
+#pragma config WRT = WRT_upper       // UserNVM self-write protection bits (0x0000 to 0x01FF write protected)
+#pragma config SCANE = not_available // Scanner Enable bit (Scanner module is not available for use)
+#pragma config LVP = ON              // Low Voltage Programming Enable bit (Low Voltage programming enabled. MCLR/Vpp pin function is MCLR.)
+
+// CONFIG5
+#pragma config CP = ON   // UserNVM Program memory code protection bit (Program Memory code protection enabled)
+#pragma config CPD = OFF // DataNVM code protection bit (Data EEPROM code protection disabled)
+
+// #pragma config statements should precede project file includes.
+// Use project enums instead of #define for ON and OFF.
+#endif
+
+/*
+ * ISR definition 
+ */
+    int i;
+    int j;
+    int k;
+void __interrupt() isr(void)
+{
+    /*
+        interrupt sub program
+    */
+    PIR0bits.TMR0IF = 0x00; // clear TMR0 register
+    PORTC = ~PORTC;         // flip PORTC
+}
+
+void setup(void)
+{
+    /***
+    init PORTC
+    ***/
+    ANSELC = 0x00;
+    LATC = 0x00;
+    TRISC = 0x00;
+    PORTC = 0x00;
+
+    /***
+    init interrupts
+    ***/
+    INTCONbits.GIE = 1;    // enable       global interrupt
+    INTCONbits.PEIE = 0;   // disable      peripheral interrupt
+    INTCONbits.INTEDG = 1; // rising edge  interrupt
+    PIE0bits.TMR0IE = 1;   // enable       Timer0 interrupt
+
+    /***
+    init TMR0
+    ***/
+    TMR0H = 0xF3;
+    TMR0L = 0x00;
+    PIR0bits.TMR0IF = 0x00;
+
+    T0CON0 = 0xCF;
+    T0CON1 = 0x47;
+
+}
+void loop(void)
+{
+    while(++i)
+    {
+        while(++j)
+        {
+            while(++k)
+            {
+                
+            }
+        }
+    }
+}
+void main(void)
+{
+    setup();
+    while (1)
+    {
+        loop();
+    }
+    return;
+}
